ded90979dc4a4c4e32fb57349d577a6ecc8027c3
Calvest <frankiewb9527@gmail.com>
2022-03-14 19:26:38 +0800

    使用 ts 重写 lodash-chunk 函数

--- lodash/ts/chunk.ts
+++ lodash/ts/chunk.ts
@@ -1,9 +1,9 @@
 /*
  * @Author: Calvest
- * @Date: 2022-03-13 12:17:44
+ * @Date: 2022-03-14 13:59:38
  * @LastEditors: Calvest
- * @LastEditTime: 2022-03-14 14:10:06
- * @FilePath: /training/Lodash/js/chunk.js
+ * @LastEditTime: 2022-03-14 16:36:03
+ * @FilePath: /training/lodash/ts/chunk.ts
  */
 /**
  * @description: 将数组 array 拆分成多个 size 长度的区块，并将这些区块组成一个新数组。 如果 array 无法被分割成全部等长的区块，那么最后剩余的元素将组成一个区块。 
@@ -11,19 +11,19 @@
  * @param {Number} size
  * @return {Array} array
  */
-function chunk(array = [], size = 1) {
+function chunk(array: any[], size: number = 1): any[] {
     if (!Array.isArray(array)) {
-        throw new Error("process(): Argument must be an array.");
+        throw new Error('process(): Argument must be an array.');
     } else {
         if (!array.length) {
-            return array;
+            return [];
         } else {
             if (size > 0 && Number.isInteger(size)) {
                 if (size >= array.length) {
                     return array;
                 } else {
-                    let counter = 0;
-                    let result = new Array(Math.ceil(array.length / size));
+                    let counter: number = 0;
+                    let result: any[] = new Array(Math.ceil(array.length / size));
 
                     for (let i = 0; i < result.length; i++) {
                         result[i] = array.slice(counter, counter += size);
@@ -32,7 +32,7 @@ function chunk(array = [], size = 1) {
                     return result;
                 }
             } else {
-                throw new Error("process(): Argument must be an positive integer.")
+                throw new Error('process(): Argument must be an positive integer.');
             }
         }
     }
